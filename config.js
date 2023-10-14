d704779bd4c3b88664ca9479c6b25a42f03aebe1
imranh1987 <md.imranh1987@gmail.com>
2023-10-14 15:58:23 +0600

    upload files with fetching functionality with paging

---
+++ config.js
@@ -0,0 +1,11 @@
+const config = {
+    db: {
+        host: "localhost",
+        user: "root",
+        password: "123456",
+        database: "api_v2",
+        connectTimeout: 60000
+    },
+    listPerPage: 10,
+};
+module.exports = config;

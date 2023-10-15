abcb2a802258fc21629719337748759eae63a11e
imranh1987 <md.imranh1987@gmail.com>
2023-10-15 12:19:59 +0600

    add create api

--- config.js
+++ config.js
@@ -8,4 +8,5 @@ const config = {
     },
     listPerPage: 10,
 };
+
 module.exports = config;
--- routes/programmingLanguages.js
+++ routes/programmingLanguages.js
@@ -9,6 +9,15 @@ router.get("/", async function(req, res, next) {
         console.error(`Error while getting programming languages `, err.message);
         next(err);
     }
-})
+});
+
+router.post('/', async function (req, res, next) {
+   try {
+       res.json(await programmingLanguages.create(req.body));
+   } catch (err) {
+       console.error(`Error while creating programming language`, err.message);
+       next(err);
+   }
+});
 
 module.exports = router;
